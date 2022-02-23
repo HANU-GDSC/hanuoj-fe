a6764113ae323c1d5c7e1f93ef9c17198cc7458b
Lê Lâm <89892749+Liamm999@users.noreply.github.com>
2022-02-23 16:34:06 +0700

    trả lại file gốc do vừa nãy commit nhầm.
    sorry anh em nãy commit nhầm cả file này :'((

--- src/views/AuthLogin.vue
+++ src/views/AuthLogin.vue
@@ -1,20 +1,11 @@
 <template>
     Login
-    <Button text="Save" des="submit form" :disable="true"/><br>
-    <Button text="Yes" type="primary" des="accept"/><br>
-    <Button text="No" type="secondary" des="refuse"/>
 </template>
 
 <script>
-import Button from '../components/general/Button';
 export default {
-  components: { Button },
     name: "AuthLogin",
 };
 </script>
 
 <style>
-Button {
-    margin-left: 50%;
-}
-</style>
