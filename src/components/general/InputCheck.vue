02a9925b3f57fd6157233d0e4d3d296b3d134a6e
Liamm999 <lelam96705@gmail.com>
2022-02-26 22:50:25 +0700

    InputCheck first commit

---
+++ src/components/general/InputCheck.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="input-form">
+    <div v-for="(value, index) in values" :key="index">
+      <input
+        type="checkbox"
+        :id="index"
+        v-model="inputData"
+        :value="value.value"
+        :disabled="disable"
+        :required="require"
+        :checked="checked | value.value"
+        @change="updateData(inputData)"
+      />
+      <label :for="index">{{ value.value }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputCheck",
+  props: {
+    values: {
+      type: Array,
+      required: true,
+    },
+    checked: {
+      type: Array,
+      default: [],
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    require: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      inputData: [],
+      isChecked: false,
+    };
+  },
+
+  created() {
+    return (this.inputData = this.checked);
+  },
+
+  methods: {
+    updateData(inputData) {
+      this.$emit("dataUpdated", inputData);
+    },
+    checked() {
+      return this.isChecked;
+    },
+  },
+};
+</script>
+
+<style></style>
