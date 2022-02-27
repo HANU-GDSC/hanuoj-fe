84e8e07143e7fc8a4ac9069c3fa06542fd13650a
Condition for project GDSC <ducan.co.6789@gmai.com>
2022-02-27 09:47:22 +0700

    An first commit

---
+++ src/components/general/InputPass.vue
@@ -0,0 +1,41 @@
+<template>
+  <input type="password" :value="value" placeholder="enter your pass" />
+</template>
+
+<script>
+export default {
+  name: "InputPass",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    require: {
+      type: Boolean,
+      default: true,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      password: this.password,
+    };
+  },
+  methods: {
+    dataUpdated(event) {
+      this.password = event.target.value;
+      this.$emit("dataUpdated", this.password);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
