4ea9d898a5c2aa940f2399eb746c58969a6eab11
Duc Ta <taminhduc28092000@gmail.com>
2022-03-05 15:09:03 +0700

    Input Num + Input Text

--- src/components/general/InputNum.vue
+++ src/components/general/InputNum.vue
@@ -33,8 +33,8 @@ export default {
     },
   },
   methods: {
-    dataUpdated() {
-      this.$emit("dataUpdated", this.value);
+    dataUpdated(event) {
+      this.$emit("dataUpdated", event.target.value);
     },
   },
 };
