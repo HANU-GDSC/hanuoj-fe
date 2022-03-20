a2cb577d6c3e5707b33378b4666851b5d6af11d6
lamlam-l <lam.2002.2015@gmail.com>
2022-03-20 13:25:10 +0700

    fix router watcher bug

--- src/helpers/routerWatcher.js
+++ src/helpers/routerWatcher.js
@@ -2,7 +2,7 @@ import router from "../router/index";
 
 export default function (to, from) {
     const accessToken = localStorage.getItem("accessToken");
-    if (to.meta.type === "protected" && !accessToken)
+    if ((to.meta.type !== "public" && to.meta.type !== "auth") && !accessToken)
         return router.push({ name: "Login" });
 
     if (to.meta.type === "auth" && accessToken)
