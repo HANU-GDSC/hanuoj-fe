1d4427fde806a4fc5ba396a6a0dee2244c28131a
quanvda <quandang2102@gmail.com>
2022-04-03 23:54:38 +0700

    contest

---
+++ src/model/contest/contest.js
@@ -0,0 +1,89 @@
+const apiService = require('../../helpers/apiService')
+const { Problem } = require('./problem')
+
+class Contest {
+  constructor({
+    id,
+    name,
+    description,
+    startAt,
+    endAt,
+    author,
+    problems
+  }) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.startAt = startAt
+    this.endAt = endAt
+    this.author = author
+    this.problems = problems
+  }
+
+  static init() {
+    return new Contest({
+      id: undefined,
+      name: undefined,
+      description: undefined,
+      startAt: undefined,
+      endAt: undefined,
+      author: undefined,
+      problems: []
+    })
+  }
+
+  async create () {
+    if (!this.name) {
+      throw new Error('Name is required')
+    }
+    if (!this.description) {
+      throw new Error('Description is required')
+    }
+    if (!this.startAt) {
+      throw new Error('Start At is required')
+    }
+    // Add more checks here to alert user
+    const id = await apiService('POST', '/contest/contest', {}, {
+      name: this.name,
+      description: this.description,
+      startAt: this.startAt,
+      endAt: this.endAt,
+      problems: this.problems
+    })
+    this.id = id
+  }
+
+  addProblem(problem) {
+    if (!(problem instanceof Problem)) {
+      throw new Error('Argument "problem" must be of type "Problem"')
+    }
+    this.problems.push(problem)
+  }
+
+  setStartAt(date) {
+    if (!(date instanceof Date)) {
+      throw new Error('Argument "date" must be of type "Date"')
+    }
+    if (date.getTime() <= new Date().getTime()) {
+      throw new Error('Start at must be later than now')
+    }
+    this.startAt = date
+  }
+
+  setEndAt(date) {
+    if (!(date instanceof Date)) {
+      throw new Error('Argument "date" must be of type "Date"')
+    }
+    if (date.getTime() <= new Date().getTime()) {
+      throw new Error('End at must be later than now')
+    }
+    if (this.startAt !== undefined && date.getTime() <= this.startAt) {
+      throw new Error('End at must be later than start at')
+    }
+    this.endAt = date
+  }
+}
+
+module.exports = {
+  Contest
+}
---
+++ src/model/contest/problem.js
@@ -0,0 +1,15 @@
+class Problem {
+  constructor({
+    ordinal,
+    coreProblemId,
+    score
+  }) {
+    this.ordinal = ordinal
+    this.coreProblemId = coreProblemId
+    this.score = score
+  }
+}
+
+module.exports = {
+  Problem
+}
