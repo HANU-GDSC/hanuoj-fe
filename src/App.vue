e8b004a155036c4300f36cb93767d80cc9d26505
lamlam-l <lam.2002.2015@gmail.com>
2022-05-11 10:48:34 +0700

    update background

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <!-- thẻ main có class là dark hoặc light, css màu sắc của các phần tử con đều phải dựa theo class chính này -->
-  <div id="main" :class="$store.state.general.theme">
-    <Nav />
-    <div class="content">
-      <router-view />
+    <!-- thẻ main có class là dark hoặc light, css màu sắc của các phần tử con đều phải dựa theo class chính này -->
+    <div id="main" :class="$store.state.general.theme">
+        <Nav />
+        <div class="content">
+            <router-view />
+        </div>
+        <AlertBox
+            :isShow="$store.state.general.alert.isAlert"
+            :text="$store.state.general.alert.alertMessage"
+            :type="$store.state.general.alert.alertType"
+            @close="$store.dispatch('general/closeAlert')"
+        />
     </div>
-    <AlertBox
-      :isShow="$store.state.general.alert.isAlert"
-      :text="$store.state.general.alert.alertMessage"
-      :type="$store.state.general.alert.alertType"
-      @close="$store.dispatch('general/closeAlert')"
-    />
-  </div>
 </template>
 <script>
 import routerWatcher from "./helpers/routerWatcher";
@@ -22,47 +22,46 @@ import { login } from "./model/CoderAuth/domainLogic/User";
 import { deleteExpiredCode } from "./helpers/localStorage";
 
 export default {
-  name: "App",
-  data() {
-    return {};
-  },
-  components: {
-    Nav,
-    AlertBox,
-  },
-  computed: {
-    setBackGround() {
-      return this.$store.state.general.theme;
+    name: "App",
+    data() {
+        return {};
     },
-  },
-
-  async created() {
-    // local storage setup
-    this.$store.dispatch("general/initTheme");
-    this.$store.dispatch("general/setLanguage");
-    deleteExpiredCode();
-
-  },
-
-  watch: {
-    $route(to, from) {
-      routerWatcher(to, from);
+    components: {
+        Nav,
+        AlertBox,
     },
-    setBackGround() {
-      document.body.style.backgroundColor =
-        this.$store.state.general.theme === "dark-theme"
-          ? "hsl(230, 28%, 12%)"
-          : "hsl(230, 60%, 99%)";
+    computed: {
+        setBackGround() {
+            return this.$store.state.general.theme;
+        },
     },
-  },
+
+    async created() {
+        // local storage setup
+        this.$store.dispatch("general/initTheme");
+        this.$store.dispatch("general/setLanguage");
+        deleteExpiredCode();
+    },
+
+    watch: {
+        $route(to, from) {
+            routerWatcher(to, from);
+        },
+        setBackGround() {
+            document.body.style.backgroundColor =
+                this.$store.state.general.theme === "dark-theme"
+                    ? "#4F3B78"
+                    : "#fff";
+        },
+    },
 };
 </script>
 <style lang="scss">
 @import "./App.scss";
 #main {
-  display: flex;
+    display: flex;
 }
 .content {
-  flex-grow: 1;
+    flex-grow: 1;
 }
 </style>
